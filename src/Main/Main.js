bf483284a07fd96d5778d2206f4ec57ec49f0d28
mattdean <matthewallandean@gmail.com>
2021-01-29 13:16:34 -0700

    Write inital structure

---
+++ src/Main/Main.js
@@ -0,0 +1,18 @@
+import './Main.css'
+
+function Main() {
+  return (
+    <main>
+      <h1 className="main-title">
+        Services I Offer
+      </h1>
+      <article className="main-body">
+        <p>Ecommerce Integration</p>
+        <p>Graphic Web Design</p>
+        <p>Content Management Systems</p>
+      </article>
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/Nav/Nav.js
@@ -0,0 +1,19 @@
+import './Nav.css'
+
+function Nav() {
+  return (
+    <nav>
+      <p className="button top">
+        logo
+      </p>
+      <p className="button services">
+       logo
+      </p>
+      <p className="button contact">
+        logo
+      </p>
+    </nav>
+  );
+}
+
+export default Nav;
